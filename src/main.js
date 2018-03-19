4b7fa2b799ccf7d680689fc81b2ff3de62499cad
MarcusVirg <marcusvirg@yahoo.com>
2018-03-19 18:51:50 -0500

    Started to implement Semantic UI framework.

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import SuiVue from 'semantic-ui-vue'
 
 Vue.config.productionTip = false
 
@@ -13,3 +14,5 @@ new Vue({
   template: '<App/>',
   components: { App }
 })
+
+Vue.use(SuiVue)
