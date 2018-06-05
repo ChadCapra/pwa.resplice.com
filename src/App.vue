9ec3cea4b24aae38f0e306da1a5aa48693484383
Marcus Virginia <marcusvirg@yahoo.com>
2018-06-04 19:32:30 -0500

    Added transition for router and global style for buttons and span

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <main>
-      <router-view style="height: 100vh;"></router-view>
+      <transition>
+        <router-view style="height: 100vh;"></router-view>
+      </transition>
     </main>
   </div>
 </template>
@@ -33,5 +35,15 @@ main {
 
 a {
   color:bisque;
+}
+button, span {
+  outline: 0;
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  -webkit-tap-highlight-color: transparent; 
 }
 </style>
