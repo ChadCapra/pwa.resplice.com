ac1327842a93e7efc4cb5e27206b7e1f8a690002
Marcus Virginia <marcus@Marcuss-MacBook-Pro.local>
2018-06-07 16:45:22 -0500

    Started layout for the groups page

--- src/contacts/Groups.vue
+++ src/contacts/Groups.vue
@@ -1,3 +1,48 @@
 <template>
-  <h1>Groups Page Coming Soon</h1>
+  <div v-if="groups.length > 0" class="groups">
+    <el-card v-for="group in groups" :key="group.id" class="group" shadow="hover">
+      <div class="g-header">
+        <h1>{{ group.name }}</h1>
+      </div>
+      <div class="g-description">{{ group.description }}</div>
+      <div class="g-buttons">
+        <el-button v-if="!group.shared" type="primary">Share</el-button>
+        <el-button type="danger">Delete</el-button>
+      </div>
+    </el-card>
+  </div>
+  <div v-else class="none">
+    <h1>You have no groups 😥<br>Click below to create one:</h1>
+    <el-button type="primary">Create New Group</el-button>
+  </div>
 </template>
+
+<script>
+export default {
+  computed: {
+    groups () {
+      return this.$store.getters.getGroups
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.none {
+  display: flex;
+  flex-direction: column;
+}
+.groups {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+.group {
+  margin: 15px;
+}
+.g-buttons {
+  display: flex;
+  justify-content: center;
+  margin-top: 15px;
+}
+</style>
