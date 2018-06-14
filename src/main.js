d27c09cc271eea3aceaf574e0e18fdc0d04d6774
Marcus Virginia <marcus@Marcuss-MacBook-Pro.local>
2018-06-13 19:07:41 -0500

    Added cloudinary and croppie packages

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+
 // Import custom theme configured from element-variables.scss
 import '../theme/index.css'
+
 // Import Element CSS framework
 import ElementUI from 'element-ui'
 import locale from 'element-ui/lib/locale/lang/en'
+
 // Import needed FontAwesome logos and Icon component
 import 'vue-awesome/icons/envelope'
 import 'vue-awesome/icons/phone'
@@ -33,14 +36,20 @@ import 'vue-awesome/icons/calendar'
 import 'vue-awesome/icons/lock'
 import 'vue-awesome/icons/times'
 import Icon from 'vue-awesome/components/Icon'
+
 // Import vue-touch for phone gestures
 import VueTouch from 'vue-touch'
+
+// Import croppie for cropping photos
+import VueCroppie from 'vue-croppie'
+
 // Import Vue.js App component, router component, and VueX for state management
 import App from './App'
 import router from './router'
 import store from './store/store'
 
 Vue.config.productionTip = false
+Vue.use(VueCroppie)
 Vue.use(VueTouch, {name: 'v-touch'})
 Vue.use(ElementUI, {locale})
 Vue.component('icon', Icon)
