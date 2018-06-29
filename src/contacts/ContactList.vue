b6a868b156af1ebcb2a22afceaa0df60d61b892a
Marcus Virginia <marcus@Marcuss-MacBook-Pro.local>
2018-06-29 10:45:51 -0500

    Improved new contact section

--- src/contacts/ContactList.vue
+++ src/contacts/ContactList.vue
@@ -3,10 +3,14 @@
     <re-loading></re-loading>
   </div>
   <div v-else>
-    <div v-if="contacts">
+    <div v-if="contacts.length > 0">
       <re-contact v-for="contact in contacts" :contact="contact" :key="contact.id"></re-contact>
     </div>
-    <div v-else>You have no contacts</div>
+    <div class="no-contact" v-else>
+      <p>You have no one sharing their attributes with you 😰</p>
+      <p>Share your attributes with more people to get shares back!</p>
+      <el-button type="primary" @click="$router.push({name: 'Share'})">Share</el-button>
+    </div>
   </div>
 </template>
 
