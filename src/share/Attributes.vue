015bae16b5b612f7de886a0c46dd91a15ecdca64
Marcus Virginia <marcus@Marcuss-MacBook-Pro.local>
2018-06-29 15:07:43 -0500

    Added attributes with tree element

---
+++ src/share/Attributes.vue
@@ -0,0 +1,118 @@
+<template>
+  <div class="attributes">
+    <div class="top">
+      <h1>Attributes</h1>
+      <h2>Sharing with {{ sharingContacts.length }} people</h2>
+      <p>Select the attributes you would like to share:</p>
+    </div>
+    <div class="body">
+      <el-tree
+        :data="data2"
+        show-checkbox
+        node-key="id"
+        :props="defaultProps">
+      </el-tree>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      data2: [{
+        id: 1,
+        label: 'Level one 1',
+        children: [{
+          id: 4,
+          label: 'Level two 1-1',
+          children: [{
+            id: 9,
+            label: 'Level three 1-1-1'
+          }, {
+            id: 10,
+            label: 'Level three 1-1-2'
+          }]
+        }]
+      }, {
+        id: 2,
+        label: 'Level one 2',
+        children: [{
+          id: 5,
+          label: 'Level two 2-1'
+        }, {
+          id: 6,
+          label: 'Level two 2-2'
+        }]
+      }, {
+        id: 3,
+        label: 'Level one 3',
+        children: [{
+          id: 7,
+          label: 'Level two 3-1'
+        }, {
+          id: 8,
+          label: 'Level two 3-2'
+        }]
+      }],
+      defaultProps: {
+        children: 'children',
+        label: 'label'
+      }
+    }
+  },
+  computed: {
+    sharingContacts () {
+      return this.$store.getters.getSharingContacts
+    },
+    attributes () {
+      return this.$store.getters.getUserAttributes
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .attributes {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+  }
+  .attributes > * {
+    flex-shrink: 0;
+  }
+  .top {
+    color: #1BBC9B;
+    background-color: #fff;
+  }
+  .body {
+    color: white;
+    background-color: #1BBC9B;
+    flex-grow: 1;
+    padding: 25px;
+    display: flex;
+    flex-direction: column;
+  }
+  h1 {
+    font-size: 78px;
+    margin: 0;
+  }
+  h2 {
+    font-size: 36px;
+    margin: 5px;
+  }
+  @media screen and (max-width: 350px){
+    h1 {
+      font-size: 62px;
+      margin: 0;
+    }
+    h2 {
+      font-size: 20px;
+      margin: 5px;
+    }
+  }
+  .el-tree {
+    color:#FFF;
+    background: transparent;
+  }
+</style>
