0dfe548ef8dd0eaca3b8c3f6b449ebaed504044e
Marcus Virginia <marcusvirg345@gmail.com>
2019-01-17 01:50:51 -0600

    Added onclick callback to buttons

--- src/Components/Buttons/ReButton.js
+++ src/Components/Buttons/ReButton.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import './button.scss'
 
-const ReButton = ({ type, text, loading, width }) => {
+const ReButton = ({ type, text, loading, width, onClick }) => {
   if (loading) {
     return <button className={`btn btn--${type}`}>Loading...</button>
   }
 
   return (
-    <button style={{ width }} className={`btn btn--${type}`}>
+    <button style={{ width }} className={`btn btn--${type}`} onClick={onClick}>
       {text}
     </button>
   )
