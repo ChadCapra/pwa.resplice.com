c2f130cb3a4d04119b2d969afff71d9fa804e212
Marcus Virginia <marcusvirg345@gmail.com>
2019-01-19 14:56:43 -0600

    Changed fetch_types to fetch_settings

--- src/reducers/attributesReducer.js
+++ src/reducers/attributesReducer.js
@@ -1,4 +1,4 @@
-import { FETCH_TYPES } from '../actions/types'
+import { FETCH_SETTINGS } from '../actions/types'
 
 const INITIAL_STATE = {
   types: []
@@ -6,8 +6,8 @@ const INITIAL_STATE = {
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case FETCH_TYPES:
-      return { ...state, types: action.payload }
+    case FETCH_SETTINGS:
+      return { ...state, types: action.payload.attribute_types }
     default:
       return state
   }
