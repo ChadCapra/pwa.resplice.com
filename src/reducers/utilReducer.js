567f8e4a1bc00e749ae1ed117d8ff434a49bbda8
Marcus Virginia <marcusvirg345@gmail.com>
2019-01-19 14:55:09 -0600

    Utils state now gets other settings

--- src/reducers/utilReducer.js
+++ src/reducers/utilReducer.js
@@ -1,9 +1,14 @@
-import { SWIPED } from '../actions/types'
+import { SWIPED, FETCH_SETTINGS } from '../actions/types'
 
 export default (state = { swipeIndex: 0 }, action) => {
   switch (action.type) {
     case SWIPED:
       return { ...state, swipeIndex: action.payload }
+    case FETCH_SETTINGS:
+      return {
+        ...state,
+        registrationExpirySec: action.payload.registration_expiry_seconds
+      }
     default:
       return state
   }
