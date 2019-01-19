c244c77646251169018b29f9f9d1feff9e72d064
Marcus Virginia <marcusvirg345@gmail.com>
2019-01-19 14:54:56 -0600

    Added loading states for some components

--- src/Components/Buttons/ReButton.js
+++ src/Components/Buttons/ReButton.js
@@ -3,12 +3,17 @@ import './button.scss'
 
 const ReButton = ({ type, text, loading, width, onClick }) => {
   if (loading) {
-    return <button className={`btn btn--${type}`}>Loading...</button>
+    return (
+      <button style={{ width }} className={`btn btn--${type} btn--loading`}>
+        <span>{text}</span>
+        <div className="re-ring re-spin" />
+      </button>
+    )
   }
 
   return (
     <button style={{ width }} className={`btn btn--${type}`} onClick={onClick}>
-      {text}
+      <span>{text}</span>
     </button>
   )
 }
