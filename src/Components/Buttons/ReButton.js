820f01371fc1f9eec35bc28f3e8995cfafdc0f88
Marcus Virginia <marcusvirg345@gmail.com>
2019-03-17 15:33:22 -0500

    Added proptypes and documentation support

--- src/Components/Buttons/ReButton.js
+++ src/Components/Buttons/ReButton.js
@@ -1,6 +1,10 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import './button.scss'
 
+/**
+ * Primary Button for Resplice. Used on forms, in modals, etc.
+ */
 const ReButton = ({ type, text, loading, width, onClick }) => {
   if (loading) {
     return (
@@ -16,6 +20,14 @@ const ReButton = ({ type, text, loading, width, onClick }) => {
       <span>{text}</span>
     </button>
   )
+}
+
+ReButton.propTypes = {
+  type: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  loading: PropTypes.bool,
+  width: PropTypes.string,
+  onClick: PropTypes.func
 }
 
 export default ReButton
