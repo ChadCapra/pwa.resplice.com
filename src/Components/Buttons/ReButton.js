d68614f18eccbfe70b7209889cd893b16cc35348
Marcus Virginia <marcusvirg345@gmail.com>
2019-05-24 10:10:06 -0500

    Changed button on click animation

--- src/Components/Buttons/ReButton.js
+++ src/Components/Buttons/ReButton.js
@@ -6,18 +6,14 @@ import './button.scss'
  * Primary Button for Resplice. Used on forms, in modals, etc.
  */
 const ReButton = ({ type, text, loading, width = '300px', onClick }) => {
-  if (loading) {
-    return (
-      <button style={{ width }} className={`btn btn--${type} btn--loading`}>
-        <span>{text}</span>
-        <div className="re-ring re-spin" />
-      </button>
-    )
-  }
-
   return (
-    <button style={{ width }} className={`btn btn--${type}`} onClick={onClick}>
-      <span>{text}</span>
+    <button
+      style={{ width }}
+      className={`btn btn--${type}${loading ? ' btn--loading' : ''}`}
+      onClick={onClick}
+    >
+      {text}
+      {loading && <div className="re-ring re-spin" />}
     </button>
   )
 }
