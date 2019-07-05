b3880dc1b5f0b0918db1db39d3c9d3e1e2c8dc8d
Marcus Virginia <marcusvirg345@gmail.com>
2019-07-05 18:54:07 -0500

    Added bulk share route

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 
 import ReHomeView from './components/ReHomeView'
 import ReContactView from './components/ReContactView'
 import ReUserView from './components/ReUserView'
 import ReShareView from './components/ReShareView'
 import ReGroupView from './components/ReGroupView'
+import ReBulkShare from './components/Share/ReBulkShare'
 
 import './App.scss'
 
 const App = () => (
   <div className="App">
-    <Router>
-      <>
-        <Route path="/" exact component={ReHomeView} />
-        <Route path="/contact/:uuid" component={ReContactView} />
-        <Route path="/profile" exact component={ReUserView} />
-        <Route path="/share" exact component={ReShareView} />
-        <Route path="/group/:uuid" component={ReGroupView} />
-      </>
-    </Router>
+    <>
+      <Route path="/" exact component={ReHomeView} />
+      <Route path="/contact/:uuid" component={ReContactView} />
+      <Route path="/profile" exact component={ReUserView} />
+      <Route path="/share" exact component={ReShareView} />
+      <Route path="/share/bulk" exact component={ReBulkShare} />
+      <Route path="/group/:uuid" component={ReGroupView} />
+    </>
   </div>
 )
 
