4e46704c6a17fab27761ead86271dd17cfe43c48
Marcus Virginia <marcusvirg345@gmail.com>
2020-02-15 19:05:38 -0600

    Setting baseurl back to prod

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,11 +1,9 @@
 import axios from 'axios'
 
-// const baseURL =
-//   process.env.NODE_ENV === 'production'
-//     ? 'https://api.resplice.com/api'
-//     : 'http://localhost:4000/api'
-
-const baseURL = 'http://localhost:4000/api'
+const baseURL =
+  process.env.NODE_ENV === 'production'
+    ? 'https://api.resplice.com/api'
+    : 'http://localhost:4000/api'
 
 export const mockApi = axios.create({
   baseURL: 'https://ee53bf02-bbee-4b58-ae55-97f543eee1e0.mock.pstmn.io/api'
