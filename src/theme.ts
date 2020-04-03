ce0c1efb890fb435e53559e7621daf6535b01d8c
Marcus Virginia <marcusvirg@yahoo.com>
2020-04-02 23:55:10 -0500

    Fixed theme

--- src/theme.ts
+++ src/theme.ts
@@ -1,4 +1,4 @@
-const theme = {
+export default {
   colors: {
     'brand-primary': '#1bbc9b',
     'brand-primary-15': 'rgba(27, 188, 155, 0.15)',
@@ -9,9 +9,7 @@ const theme = {
     success: '#1bbc9b',
     warning: '#ffdd57',
     danger: '#ff3860',
-    'black-50': '#0000007e',
-    'mobile-max-width': '575px'
-  }
+    'black-50': '#0000007e'
+  },
+  'mobile-max-width': '575px'
 }
-
-export default theme
